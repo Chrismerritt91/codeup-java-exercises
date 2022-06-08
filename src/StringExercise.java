219229dab672b4f1566ccab5f6867ce295ff24e6
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-06-08 16:58:44 -0500

    complete exercises for strings

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String wall = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(wall);
+
+        String checkIt = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkIt);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backSlash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backSlash);
+    }
+}
